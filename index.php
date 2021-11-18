752dc6dd85655bcbfee244cf229c0c5cf95bcd17
Trần văn trung <trungtv0910@gmail.com>
2021-11-18 07:31:41 +0700

    update thu vien lay lai mat khau

--- index.php
+++ index.php
@@ -1,41 +1,43 @@
-<?php 
-    include_once 'view/header.php';
-    include_once 'model/pdo.php';
-    include_once 'model/account_model.php';
-    include_once 'model/global.php';
-    include_once 'model/customer.php';
+<?php
+
+
+
+
+include_once 'view/header.php';
+include_once 'model/pdo.php';
+include_once 'model/account_model.php';
+include_once 'model/global.php';
+include_once 'model/customer.php';
+
 ?>
 
-<?php 
-    if(isset($_GET['act'])){
-        $path =$_GET['act'];
-        switch ($path){
-            case "login":{
-                if(isset($_POST['login'])){
-                    $username=$_POST['username'];
-                    $password =$_POST['password'];
-                    $res= checkLogin($username,$password);
-                    if($res==1){
-                    
-                      echo '<script>window.location="index.php" </script>';
-                    }else 
-                    {
-                    echo "<script>Swal.fire({icon: 'error', title: 'Đăng nhập thất bại',text: 'Bạn nhập sai mật khẩu hoặc tài khoản!' })</script>";
-                           include_once 'view/home.php';
+<?php
+if (isset($_GET['act'])) {
+    $path = $_GET['act'];
+    switch ($path) {
+        case "login": {
+                if (isset($_POST['login'])) {
+                    $username = $_POST['username'];
+                    $password = $_POST['password'];
+                    $res = checkLogin($username, $password);
+                    if ($res == 1) {
+
+                        echo '<script>window.location="index.php" </script>';
+                    } else {
+                        echo "<script>Swal.fire({icon: 'error', title: 'Đăng nhập thất bại',text: 'Bạn nhập sai mật khẩu hoặc tài khoản!' })</script>";
+                        include_once 'view/home.php';
                     }
-            
                 }
-
-
-            }break;
-            case "register":{
-                if(isset($_POST['register'])){
+            }
+            break;
+        case "register": {
+                if (isset($_POST['register'])) {
                     $email = $_POST['email'];
                     $custName = $_POST['name'];
                     $password = $_POST['password'];
                     $passwordCheck = $_POST['passwordCheck'];
                     $username = $_POST['username'];
-                    if($password == $passwordCheck){
+                    if ($password == $passwordCheck) {
                         add_customer($email, $custName, $username, $password);
                         echo "<script>
                                 Swal.fire({
@@ -45,25 +47,24 @@
                                 showConfirmButton: false,
                                 timer: 1500})
                             </script>";
-                           include_once 'view/home.php';
-                    }else{
+                        include_once 'view/home.php';
+                    } else {
                         echo "<script>Swal.fire({icon: 'error', title: 'Nhập lại mật khẩu không chính xác !'})</script>";
                         include_once 'view/home.php';
                     }
                 }
+            }
+            break;
 
-
-            }break;
-
-            case "change-pass":{
-                if(isset($_POST['changepass']) && ($_POST['changepass'])){
+        case "change-pass": {
+                if (isset($_POST['changepass']) && ($_POST['changepass'])) {
                     $custId = $_POST['custId'];
                     $username = $_POST['username'];
                     $password = $_POST['password'];
                     $passwordCheck = $_POST['passwordCheck'];
-                    if($password == $passwordCheck){
+                    if ($password == $passwordCheck) {
                         update_password($custId, $password);
-                        $_SESSION['login'] = checkLogin($username,$password);
+                        $_SESSION['login'] = checkLogin($username, $password);
                         echo "<script>
                                 Swal.fire({
                                 position: 'top-end',
@@ -72,16 +73,17 @@
                                 showConfirmButton: false,
                                 timer: 1500})
                             </script>";
-                           include_once 'view/home.php';
-                    }else{
+                        include_once 'view/home.php';
+                    } else {
                         echo "<script>Swal.fire({icon: 'error', title: 'Nhập lại mật khẩu không chính xác !'})</script>";
                         include_once 'view/home.php';
                     }
                 }
-            }break;
+            }
+            break;
 
-            case "edit-customer":{
-                if(isset($_POST['edit-customer']) && ($_POST['edit-customer'])){ 
+        case "edit-customer": {
+                if (isset($_POST['edit-customer']) && ($_POST['edit-customer'])) {
                     $custId = $_POST['custId'];
                     $username = $_POST['username'];
                     $password = $_POST['password'];
@@ -90,7 +92,7 @@
                     $email = $_POST['email'];
                     $address = $_POST['address'];
                     edit_customer($custId, $username, $password, $custName, $phone, $email, $address);
-                    $_SESSION['login'] = checkLogin($username,$password);
+                    $_SESSION['login'] = checkLogin($username, $password);
                     echo "<script>
                                 Swal.fire({
                                 position: 'top-end',
@@ -101,28 +103,56 @@
                             </script>";
                     include_once 'view/home.php';
                 }
-            }break;
-            case "logout" : {
+            }
+            break;
+        case "forget-pass": {
+                // echo '<script>alert("Xin chao các ban")</script>';
+                if (isset($_POST['forget-pass']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    // echo $_POST['email'];
+                    if (checkEmail_customer($_POST['email'])) {
+                        $res = checkEmail_customer($_POST['email']);
+                        $password=$res['password'];
+                        $email=$res['email'];
+                        $username=$res['username'];
+                       if(sendEmail($email,$password,$username)==true) {
+                        echo "<script>
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Yêu cầu lấy lại mật khẩu đã được duyệt. Vui lòng kiểm tra Email',
+                            showConfirmButton: false,
+                            timer: 2500
+                          })
+                        </script>";
+                            include 'view/home.php';
+                       }else{
+                           echo 'Thất bại';
+                           include 'view/home.php';
+                       }
+                    }
+                }
+            }
+            break;
+        case "logout": {
                 checkLogout();
                 echo '<script>window.location="index.php" </script>';
-            }break;
-            case "product":{
+            }
+            break;
+        case "product": {
                 include 'view/product.php';
             }
             break;
-            case "":{
-
+        case "": {
             }
-            default: 
+        default:
             include 'view/home.php';
             break;
-        }
-    }else{
-        include 'view/home.php';
-       
     }
+} else {
+    include 'view/home.php';
+}
 ?>
 
-<?php 
-    include 'view/footer.php';
+<?php
+include 'view/footer.php';
 ?>
--- model/customer.php
+++ model/customer.php
@@ -1,5 +1,8 @@
 <?php
 include_once 'pdo.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 function loadAll_customer(){
         $sql="SELECT * FROM tbl_customer order by custId DESC";
         $listcustomer=pdo_query($sql);
@@ -48,8 +51,49 @@ function update_password($custId, $password) {
     return $res; 
 }
 
-
-
+function checkEmail_customer($email){
+    $sql="SELECT * FROM tbl_customer where email = '$email'";
+    $res = pdo_query_one($sql);
+    return $res;
+}
+function sendEmail($email,$password,$username){
+    include 'model/library.php'; // include the library file
+    require 'vendor/autoload.php';
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = SMTP_HOST;  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = SMTP_UNAME;                 // SMTP username
+        $mail->Password = SMTP_PWORD;                           // SMTP password
+        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = SMTP_PORT;                                    // TCP port to connect to
+        //Recipients
+        $mail->setFrom(SMTP_UNAME, "VyHomeDecor");
+        $mail->addAddress($email, 'Tên người nhận');     // Add a recipient | name is option
+        $mail->addReplyTo(SMTP_UNAME, 'Tên người trả lời');
+//                    $mail->addCC('CCemail@gmail.com');
+//                    $mail->addBCC('BCCemail@gmail.com');
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = "Lấy lại mật khẩu";
+        $mail->Body = "VyhomeDecor Xin chào quý khách:
+        Yêu cầu lấy lại mật khẩu của bạn đã được chấp nhận.
+        Mật khẩu của bạn là:$password Cảm ơn quý khách.";
+        $mail->AltBody = "Mật khẩu của bạn là:0123213"; //None HTML
+        $result = $mail->send();
+    
+        if (!$result) {
+            $error = "Có lỗi xảy ra trong quá trình gửi mail";
+            return 0;
+        }
+        return 1;
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
+}
 
 
 
