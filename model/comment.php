0400a28ee6b67ac55a83497a0588b6512f052759
Alpha-Is-Me <nguyentuhuy97@gmail.com>
2021-11-24 15:59:11 +0700

    update thống kê

--- model/comment.php
+++ model/comment.php
@@ -5,7 +5,7 @@
     }
     function statistical_comments_product(){
         $sql = "SELECT tbl_product.prodName as prodName,tbl_product.prodId as prodId, tbl_product.quantity as quantity, tbl_product.price as price, tbl_product.image as image, max(tbl_comment.date) as lastDate, count(tbl_comment.commId) as countComm";
-        $sql.=" from tbl_comment left join tbl_product on tbl_productgit.prodId=tbl_comment.prodId";
+        $sql.=" from tbl_comment left join tbl_product on tbl_product.prodId=tbl_comment.prodId";
         $sql.=" group by tbl_product.prodId order by max(tbl_comment.date) desc";
         $list_comments_product = pdo_query($sql);
         return $list_comments_product;
