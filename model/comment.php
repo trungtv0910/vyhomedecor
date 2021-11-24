c9707fbbb39556e7f028a5c6cc6be8ccdf0dc90b
Trần văn trung <trungtv0910@gmail.com>
2021-11-24 15:47:59 +0700

    show comment client

--- model/comment.php
+++ model/comment.php
@@ -3,4 +3,33 @@
         $sql="INSERT INTO tbl_comment(prodId, custId, content, date) values ('$prodId', '$custId', '$content', '$date')";
         pdo_execute($sql);
     }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function load_commentByIdProd($prodId)
+    {
+        $sql ="SELECT * From tbl_comment where prodId =$prodId";
+        return pdo_query($sql);       
+    }
 ?>
