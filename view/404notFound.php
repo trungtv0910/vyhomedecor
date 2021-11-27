bc69c76767ba58109c9c9baee7f36024185259be
thanhvi22 <nguyenthithanhvi2206@gmail.com>
2021-11-27 22:32:19 +0700

    giao diện 404

---
+++ view/404notFound.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reponsive.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<style>
+    .notFound{
+        width: 100%;
+        text-align: center;
+        background-color: 
+    }
+    .title{
+        font: 5rem sans-serif;
+    }
+    .page{
+        font: 1em sans-serif;
+        margin-bottom: 20px;
+    }
+    .content{
+        font: 1em sans-serif;
+        margin-bottom: 5px;
+    }
+    .a{
+        font: 1em sans-serif;
+        
+    }
+    .a a{
+        text-decoration: none;
+        color: blue;
+    }
+    .a a:hover{
+        color: red;
+    }
+</style>
+<body>
+    <div class="notFound">
+        <div class="title">404</div>
+        <div class="page">Không Tìm Thấy Trang </div>
+        <div class="content">Có vẻ như bạn tìm thấy một trục trặc trong ma trận...</div>
+        <div class="a">
+         <a href="#"><i class="fas fa-arrow-left"></i> Quay lại bảng điều khiển</a>
+        </div>
+    </div>
+</body>
+</html>
