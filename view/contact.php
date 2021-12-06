4d2d13319fbad4e99f36a9f95950fb085a9d545c
Alpha-Is-Me <nguyentuhuy97@gmail.com>
2021-12-06 18:07:48 +0700

    update contact

--- view/contact.php
+++ view/contact.php
@@ -1,14 +1,17 @@
 <style>
    .contact{
        width: 100%;
-       height: 600px;
+       /* height: 600px; */
+       margin-bottom: 50px;
+       display: flex;
+       align-items: center;
    }
    .contact h1{
         color: #333;
         font-size: 2.4rem;
    }
    .information{
-       width: 40%;
+       width: 100%;
        float: left;
        margin-top: 10px;
    }
@@ -21,7 +24,8 @@
    }
    .thong-tin h2{
        color: #666;
-       font-size: 2rem;
+       font-size: 2.4rem;
+       margin-bottom: 24px;
    }
    .time{
        margin-top: 5%;
@@ -35,34 +39,35 @@
        font-weight: 600;
    }
    .google{
-       width: 60%;
+       width: 100%;
        float: left;
        margin-top: 10px;
    }
 </style>
 <div class="grid wide">
-    <div class="contact">
-        <h1>Liên Hệ</h1>
-        <div class="information">
-            <div class="thong-tin">
-                <h2>Thông tin liên hệ</h2>
-                <span>Công ty THHH Nội thất Vy Home Decor</span><br>
-                <span>Cửa hàng: </span>137 Nguyễn Thị Thập, Hoà Minh, Liên Chiểu, Đà Nẵng<br>
-                <span>Hotline: </span>+84 236 3710 999<br>
-                <span>Email: </span>caodangfpt.dn@fpt.edu.vn
+    <div class="row">
+        <div class="contact">
+            <div class="col l-5 information">
+                <div class="thong-tin">
+                    <h2>Thông tin liên hệ</h2>
+                    <span>Công ty THHH Nội thất Vy Home Decor</span><br>
+                    <span>Cửa hàng: </span>137 Nguyễn Thị Thập, Hoà Minh, Liên Chiểu, Đà Nẵng<br>
+                    <span>Hotline: </span>+84 236 3710 999<br>
+                    <span>Email: </span>caodangfpt.dn@fpt.edu.vn
+                </div>
+                <div class="time">
+                    <h2>Giờ làm việc</h2>
+                    <li>
+                        <span>Văn phòng: </span>Thứ 2 – Thứ 7; 9:00 – 18:00<br>
+                    </li>
+                    <li>
+                    <span>Cửa hàng: </span>Thứ 2 – Chủ Nhật; 9:00 – 21:00
+                    </li>
+                </div>
             </div>
-            <div class="time">
-                <h2>Giờ làm việc</h2>
-                <li>
-                    <span>Văn phòng: </span>Thứ 2 – Thứ 7; 9:00 – 18:00<br>
-                </li>
-                <li>
-                   <span>Cửa hàng: </span>Thứ 2 – Chủ Nhật; 9:00 – 21:00
-                </li>
+            <div class="col l-7 google">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.8024465090666!2d108.16776031400823!3d16.07573814353549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314218e6e07b1c3f%3A0x459e4bf5a2af323e!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYyDEkMOgIE7hurVuZw!5e0!3m2!1svi!2s!4v1638781350067!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
-        </div>
-        <div class="google">
-           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.8024465090666!2d108.16776031400823!3d16.07573814353549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314218e6e07b1c3f%3A0x459e4bf5a2af323e!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYyDEkMOgIE7hurVuZw!5e0!3m2!1svi!2s!4v1638781350067!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
 </div>
